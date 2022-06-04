d09a22b972a52c57edc0ba892e7473709a56b270
openmetrue <openmetrue@gmail.com>
2022-06-04 12:35:07 +0800

    Initial Commit

---
+++ pincode_checker/pincode_checkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pincode_checkerApp.swift
+//  pincode_checker
+//
+//  Created by Mark Khmelnitskii on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct pincode_checkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
